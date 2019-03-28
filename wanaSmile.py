8cfd52dd78684e0a3a5ee9d9a241c90697341528
Trieu <trieuktvn@gmail.com>
2019-03-28 11:04:26 +0700

    Update wanaSmile.py
    v1

--- wanaSmile.py
+++ wanaSmile.py
@@ -1,25 +1,35 @@
 
 import os
 
-MULT_GROUP1 = "1";
-MULT_GROUP2 = "2";
-
-a = raw_input("here: ")
+GROUP1 = "1"
+GROUP2 = "2"
+ALL = "3"
+ON = "4"
+OFF = "5"
 while True:
-	
+	print("Type \"exit\" to exit")
+	a = raw_input("Command: ")
 	if (a == "group1"):
-		print(MULT_GROUP1)
-		# os.system("echo " + MULT_GROUP1 + " > /dev/ttyS1")
-		print("echo " + MULT_GROUP1 + " > /dev/ttyS1")
-		break
+		os.system("echo " + GROUP1 + " > /dev/ttyUSB0")
+		print("echo " + GROUP1 + " > /dev/ttyUSB0")
+		
 	elif (a == "group2"):
-		print(MULT_GROUP2)
-		break
+		os.system("echo " + GROUP2 + " > /dev/ttyUSB0")
+		print("echo " + GROUP2 + " > /dev/ttyUSB0")
+	elif (a == "all"):
+		os.system("echo " + ALL + " > /dev/ttyUSB0")
+		print("echo " + ALL + " > /dev/ttyUSB0")
+	elif (a == "on"):
+		os.system("echo " + ON + " > /dev/ttyUSB0")
+		print("echo " + ON + " > /dev/ttyUSB0")
+	elif (a == "off"):
+		os.system("echo " + OFF + " > /dev/ttyUSB0")
+		print("echo " + OFF + " > /dev/ttyUSB0")
 	elif (a == "exit"):
 		break
 	else:
 		print("Invalid!Again:")
-		a = raw_input("here: ")
+		
 		
 # with serial.Serial('/dev/ttyS1', 115200, timeout=1) as ser:
 # 	# ser = serial.Serial('/dev/ttyUSB0')  # open serial port
