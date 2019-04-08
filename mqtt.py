803713f1a05ce9b96bfb979b3ea043e800e7b9a7
Trieu <trieuktvn@gmail.com>
2019-04-08 22:26:51 +0700

    Update mqtt.py

--- mqtt.py
+++ mqtt.py
@@ -18,19 +18,39 @@
 # import context  # Ensures paho is in PYTHONPATH
 import paho.mqtt.client as mqtt
 
-import logging
-logging.basicConfig(level=logging.DEBUG)
+# import logging
+# logging.basicConfig(level=logging.DEBUG)
 
 # If you want to use a specific client id, use
 # mqttc = mqtt.Client("client-id")
 # but note that the client id must be unique on the broker. Leaving the client
 # id parameter empty will generate a random id for you.
-mqttc = mqtt.Client()
 
-logger = logging.getLogger(__name__)
-mqttc.enable_logger(logger)
+def on_log(client, userdata, level, buf):
+	print("log: " + buf)
 
-mqttc.connect("m16.cloudmqtt.com", 12290, 60)
-mqttc.subscribe("$SYS/#", 0)
+def on_connect(client, userdata, flags, rc):
+	if rc == 0:
+		print ("Connect OK")
+	else:
+		print("Bad connection, return code = ", rc)
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+broker = "m16.cloudmqtt.com"
+mqttc = mqtt.Client("client")
+mqttc.username_pw_set("sknweddk", password="WDBf0aSOTwfY")
+
+mqttc.on_connect  = on_connect
+mqttc.on_log = on_log
+mqttc.on_message = on_message
+
+
+
+mqttc.connect(broker, 12290, 60)
+mqttc.subscribe("led", 0)
+# mqttc.subscribe("group", 0)
+mqttc.publish("led", "on")
 
 mqttc.loop_forever()
